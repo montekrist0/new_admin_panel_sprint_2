954e698194d732edfaa90621ebcfaa00589b45a9
montekrist0 <bogdanovigor1994@mail.ru>
2022-12-21 15:17:50 +0300

    Fix debug toolbar; it seems that changes didn't help... debug-toolbar isn't shows up although made in according with instruction

--- movies_admin/config/components/apps_middleware.py
+++ movies_admin/config/components/apps_middleware.py
@@ -13,15 +13,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 if DEBUG:
     INSTALLED_APPS += ['debug_toolbar', ]
-    MIDDLEWARE += ['django.middleware.clickjacking.XFrameOptionsMiddleware', ]
+    MIDDLEWARE = \
+        ['debug_toolbar.middleware.DebugToolbarMiddleware', ] + MIDDLEWARE

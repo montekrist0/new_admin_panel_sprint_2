d19f69adb46e1040ed5d680b8b29498441cdee2d
montekrist0 <bogdanovigor1994@mail.ru>
2022-12-22 08:02:04 +0300

    update api

--- movies_admin/movies/api/v1/views.py
+++ movies_admin/movies/api/v1/views.py
@@ -1,9 +1,38 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q
 from django.http import JsonResponse
-from django.views import View
+from django.views.generic.list import BaseListView
 
+from movies.models import Filmwork
 
-class MoviesListApi(View):
+class MoviesApiMixin:
+    model = Filmwork
     http_method_names = ['get']
 
-    def get(self, request, *args, **kwargs):
-        return JsonResponse({})
+    def get_queryset(self):
+        return # Сформированный QuerySet
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class MoviesListApi(BaseListView):
+    model = Filmwork
+    http_method_names = ['get']  # Список методов, которые реализует обработчик
+
+    def get_queryset(self):
+        return  # Сформированный QuerySet
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {
+            'results': list(self.get_queryset()),
+        }
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
+    def get_context_data(self, **kwargs):
+        return  # Словарь с данными объекта
